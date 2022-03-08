5cceb74dfee728e7b2fd4393290dcd11d2313e58
Kirill Chernikov <risk212@gmail.com>
2022-03-08 17:05:56 +0100

    fix cli tests

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -4,20 +4,18 @@ import 'package:image/image.dart';
 import 'package:ricky_cli/commands/app_icon/android/android_app_icon_controller.dart';
 import 'package:ricky_cli/commands/app_icon/ios/ios_app_icon_controller.dart';
 import 'package:ricky_cli/commands/splash/android/android_splash_controller.dart';
+import 'package:ricky_cli/commands/splash/ios/ios_splash_controller.dart';
 import 'package:ricky_cli/core/constants.dart';
+import 'package:ricky_cli/core/template/config/specitifactions/template_specification.dart';
 import 'package:ricky_cli/core/template/template.dart';
 import 'package:ricky_cli/core/template/config/template_config.dart';
-import 'package:ricky_cli/core/template/in_memory/in_memory_config_specification.dart';
 import 'package:ricky_cli/core/template/template_generator.dart';
 import 'package:test/test.dart';
 import 'package:collection/collection.dart';
 
 void main() {
   group('Android app icons tests', () {
-    //todo: mock AndroidManifest.xml
-    // test('can execute successfully with provided background in case AndroidManifest.xml is provided', () {
-    //   expectLater(AndroidAppIconController(backgroundColor: '#000000').execute(), completion(true));
-    // });
+
     final sourceImage = decodeImage(File(kSourceAppIconImagePath).readAsBytesSync());
 
     test('can execute successfully with incorrectly provided background in case AndroidManifest.xml is provided', () {
@@ -34,11 +32,6 @@ void main() {
   });
 
   group('IOS app icons tests', () {
-    //todo: mock ios folder creation
-    // test('can execute successfully with provided background and ios folder exist', () {
-    //   expectLater(IOSAppIconController(backgroundColor: '#000000').execute(), completion(true));
-    // });
-
     final sourceImage = decodeImage(File(kSourceAppIconImagePath).readAsBytesSync());
 
     test("can execute successfully with provided background and ios folder doesn't exist", () {
@@ -55,19 +48,20 @@ void main() {
   });
 
   group('IOS splash creation tests', () {
-    //todo: mock ios folder creation
-    // test('can execute successfully with provided background and ios folder exist', () {
-    //   expectLater(IOSSplashController(backgroundColor: '#000000').execute(), completion(true));
-    // });
+    final sourceImage = decodeImage(File(kSourceSplashImagePath).readAsBytesSync());
+
+    test('can execute successfully with provided background and ios folder exist', () {
+      expectLater(IOSSplashController(backgroundColor: '#000000', customSourceImage: sourceImage!).execute(), completion(true));
+    });
   });
 
   group('Structure templates tests', () {
     test('can read in memory structure config', () async {
-      final inMemoryTemplate = Template(source: InMemoryConfig(content: kBlocInMemoryConfig));
+      final inMemoryTemplate = Template(source: InMemoryConfig(content: kBlocTemplateSpecification));
 
       final structure = await inMemoryTemplate.structure;
 
-      final expectedStructure = kBlocInMemoryConfig.map((element) => TemplateElement(path: element.path, tag: '')).toList();
+      final expectedStructure = kBlocTemplateSpecification.map((element) => TemplateElement(path: element.path, tag: '')).toList();
 
       expect(const ListEquality<TemplateElement>().equals(structure, expectedStructure), true);
     });
@@ -77,15 +71,10 @@ void main() {
 
       final structure = await fileTemplate.structure;
 
-      final expectedStructure = kBlocInMemoryConfig;
+      final expectedStructure = kBlocTemplateSpecification;
 
       expect(const ListEquality<TemplateElement>().equals(structure, expectedStructure), true);
     });
-
-    //todo: implement once [WebConfig] completed.
-    // test('can read in web structure config', () async {
-    //   expect(true, true);
-    // });
   });
 
   group('Test template generator', () {
